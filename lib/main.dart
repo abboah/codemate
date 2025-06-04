cb12376f7444860ceef71d0ea0ada385183a5350
abboah <124472979+abboah@users.noreply.github.com>
2025-06-04 06:01:35 +0000

    Update main.dart
    removed api key

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   await Supabase.initialize(
     anonKey:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprZnp5cmx1aGtqcndkZGloZ3V6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NTE2MDMsImV4cCI6MjA2NDAyNzYwM30.subMzf18C58axW325rZhGWdKMEDzZzXaBiFX1pP_G0c",
+        "",
     url: "https://zkfzyrluhkjrwddihguz.supabase.co",
   );
   runApp(const MyApp());
